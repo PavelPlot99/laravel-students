6917a4e4ef8666b25d5d50904d70f42e29e72b23
Pavel <pavel.plotnikov1999@mail.ru>
2023-05-24 14:00:06 +0700

    FEAT: migrations

---
+++ database/migrations/2023_05_23_140621_create_lectures_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lectures', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('theme_id');
+            $table->unsignedBigInteger('group_id');
+
+            $table->foreign('theme_id')->references('id')->on('themes')->cascadeOnDelete();
+            $table->foreign('group_id')->references('id')->on('groups')->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lectures');
+    }
+};
